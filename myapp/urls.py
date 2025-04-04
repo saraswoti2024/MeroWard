7ec0bcf5d845d423990e938adad784a788f420e5
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2025-04-04 20:05:58 +0545

    forgot password using django auth_views

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("",home,name="home"),
@@ -14,4 +15,11 @@ urlpatterns = [
      path('log_out/',log_out,name="log_out"),
      path('view_app/',view_app,name="view_app"),
      path('emergency/',emergency,name="emergency"),
+    #for forgot password when password is not known end
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name = "auth/password_reset.html"), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name = "auth/password_reset_done.html"), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name = "auth/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name = "auth/password_reset_complete.html"), name='password_reset_complete'),
+    #for forgot password when password is not known end
 ]
+
